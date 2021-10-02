3e19a26e7396042a6fc440135a457ba56eb959af
ostatok <dev@ostatok.com>
2021-10-03 01:19:12 +0700

    use ohio

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ provider "aws" {
 
 resource "aws_instance" "example" {
 	ami = "ami-0c55b159cbfafe1f0"
-	instance_type = "t2.micro"
+	instance_type = "t3.nano"
+	tags = {
+		Name = "terraform-example"
+	}
 }
 
